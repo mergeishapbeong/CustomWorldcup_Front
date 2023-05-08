4bd2a9857f8b9bfac019c29304b0c1d9be953d16
haeun0405 <124241345+haeun0405@users.noreply.github.com>
2023-05-08 20:00:43 +0900

    feat : 메인, 플레이. 결과 페이지 뼈대 완성

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,146 @@
-import React from "react";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Play from './Play';
+import Result from './Result';
 
-function Main() {
-  return <div>Main</div>;
-}
+const Main = () => {
+  const imageSrc = "https://example.com/image.jpg";
+  const title = "예제 타이틀";
+  const description = "예제 설명";
+
+  const [page, setPage] = useState('');
+
+  const handlePlay = () => {
+    setPage('play');
+  };
+
+  const handleResult = () => {
+    setPage('result');
+  };
+
+  if (page === 'play') {
+    return <Play worldCupName={title} round={1} totalRounds={4} goToResult={handleResult} />;
+  }
+
+  if (page === 'result') {
+    return <Result />;
+  }
+
+
+  return (
+    <Container>
+      <Header>
+        <h1>커스텀 월드컵 프로그램</h1>
+      </Header>
+      <BoxContainer>
+        <ImageContainer>
+          <Image src={imageSrc} alt="image1" />
+          <Image src={imageSrc} alt="image2" />
+        </ImageContainer>
+        <Title>{title}</Title>
+        <Description>{description}</Description>
+        <ButtonContainer>
+          <PlayButton  onClick={handlePlay}>플레이</PlayButton>
+          <ResultButton onClick={handleResult}>결과 페이지</ResultButton>
+        </ButtonContainer>
+      </BoxContainer>
+    </Container>
+  );
+};
 
 export default Main;
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Header = styled.header`
+  text-align: center;
+  background-color: #282c34;
+  min-height: 10vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+  margin-bottom: 2rem;
+`;
+
+const ContentContainer = styled.main`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 20px;
+  padding: 20px;
+`;
+
+const BoxContainer = styled.div`
+  position: relative;
+  width: 374px;
+  height: 243px;
+  background: #d9d9d9;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px;
+  box-sizing: border-box;
+`;
+
+const ImageContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin-bottom: 10px;
+`;
+
+const Image = styled.img`
+  width: 180px;
+  height: 180px;
+  object-fit: cover;
+`;
+
+const Title = styled.h3`
+  margin: 0;
+  margin-bottom: 5px;
+  font-size: 18px;
+`;
+
+const Description = styled.p`
+  margin: 0;
+  margin-bottom: 10px;
+  font-size: 14px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const PlayButton = styled.button`
+  background-color: #4caf50;
+  color: white;
+  padding: 8px 16px;
+  font-size: 14px;
+  border: none;
+  cursor: pointer;
+  &:hover {
+    background-color: #45a049;
+  }
+`;
+
+const ResultButton = styled.button`
+  background-color: #008cba;
+  color: white;
+  padding: 8px 16px;
+  font-size: 14px;
+  border: none;
+  cursor: pointer;
+  &:hover {
+    background-color: #0073b7;
+  }
+`;
--- src/pages/Play.jsx
+++ src/pages/Play.jsx
@@ -1,7 +1,101 @@
-import React from "react";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
-function Play() {
-  return <div>Play</div>;
-}
+const Play = ({ worldCupName, totalRounds }) => {
+  const navigate = useNavigate();
+
+  const [selectedImage, setSelectedImage] = useState(null);
+  const images = [
+    { id: 1, src: 'https://example.com/image1.jpg', name: 'Image 1' },
+    { id: 2, src: 'https://example.com/image2.jpg', name: 'Image 2' },
+  ];
+
+  const [round, setRound] = useState(1);
+  const [roundImages, setRoundImages] = useState(images);
+
+  const handleImageClick = (image) => {
+    setSelectedImage(image);
+    if (round === totalRounds) {
+      goToResult(); // 모든 라운드가 끝난 경우 결과 페이지로 이동
+    }
+
+    setRound((prevRound) => prevRound + 1);
+
+    setRoundImages((prevRoundImages) => {
+      const newImages = prevRoundImages.map((img) => ({
+        ...img,
+        src: img.src.replace(/(\d+)/, (match) => parseInt(match) + 1),
+      }));
+      return newImages;
+    });
+  };
+
+  const goToResult = () => {
+    navigate('/result');
+  };
+
+  return (
+    <Container>
+      <ProgressInfo>
+        <h1>{worldCupName}</h1>
+        <p>{`진행 사항: ${round}/${totalRounds}`}</p>
+      </ProgressInfo>
+      <ImageContainer>
+        {roundImages.map((image) => (
+          <ClickableImage
+            key={image.id}
+            src={image.src}
+            alt={image.name}
+            onClick={() => handleImageClick(image)}
+          >
+            <ImageName>{image.name}</ImageName>
+          </ClickableImage>
+        ))}
+      </ImageContainer>
+    </Container>
+  );
+};
 
 export default Play;
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ProgressInfo = styled.div`
+  text-align: center;
+  margin-bottom: 2rem;
+`;
+
+const ImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  gap: 0;
+`;
+
+const ClickableImage = styled.div`
+  width: 329px;
+  height: 446px;
+  background-image: url(${(props) => props.src});
+  background-size: cover;
+  background-position: center;
+  cursor: pointer;
+  position: relative;
+`;
+
+const ImageName = styled.div`
+  position: absolute;
+  width: 100%;
+  text-align: center;
+  bottom: 20px;
+  color: white;
+  font-size: 24px;
+  font-weight: bold;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 5px 0;
+`;
